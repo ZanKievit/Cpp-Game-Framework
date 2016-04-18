1cc4aeba95d157cb3b47590708b09d49a161748e
Zan <zan_kievit@live.nl>
2016-04-18 10:16:45 +0200

    modified commit check

--- common/Gameobject.h
+++ common/Gameobject.h
@@ -13,9 +13,19 @@ public:
     gameobject();
     virtual ~gameobject();
     
-private:
-    void Init();
+    void addSprite(Sprite* spr);
+    void addSprite(const std::string& filename);
     
+private:
+    
+    Sprite* _sprite;
+    
+    void deleteSprite() {
+        if (_sprite != NULL) {
+            delete _sprite;
+            _sprite = NULL;
+        }
+    };
 };
 
 #endif /* Gameobject_h */
--- demo/demo.cpp
+++ demo/demo.cpp
@@ -10,7 +10,8 @@
 #include <glfw3.h>
 
 #include <common/renderer.h>
-#include <common/sprite.h>
+#include <common/Gameobject.h>
+//#include <common/sprite.h>
 #include <common/print.h>
 
 int main( void )
@@ -44,9 +45,6 @@ int main( void )
     } // Check if the ESC key was pressed or the window was closed
     while( glfwGetKey(renderer.window(), GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
           glfwWindowShouldClose(renderer.window()) == 0 );
-    
-    delete sprite1;
-    delete sprite2;
     
     // Close OpenGL window and terminate GLFW
     glfwTerminate();
